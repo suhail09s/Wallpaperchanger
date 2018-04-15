dae8b0ce5de6667f889971ebb160bd6c9437d29b
suhail09s <killer.x22@gmail.com>
2018-04-15 16:19:59 +0300

    imporved
    +Reads all wallpapers in the folder and all subfolders.
    +fixed out of index bug.
    +support unlimited number of wallpapers.
    to add:
    -customizable config file.
    -support more images formats.
    -easy pause and exit.
    better startup with windows.

--- wallpaperchanger.py
+++ wallpaperchanger.py
@@ -3,15 +3,34 @@ import os
 import threading
 import random
 import time
-
-list=os.listdir("e:\Wallpapers")
+global wall
 wall=[]
-for a in range (len(list)):
-    if str(list[a]).split('.')[-1]=='jpg' or str(list[a]).split('.')[-1]=='png':
-        wall.append(list[a])
-print (wall)
+global path
+global list
+path='E:\Wallpapers\\'
+t=0.5 # the duration in seconds
+def listing(list,v):
+    for a in range (len(list)):
+        if str(list[a]).split('.')[-1]=='jpg' or str(list[a]).split('.')[-1]=='png':
+            wall.append(v+'\\'+list[a])
+list=os.listdir(path)
+
+listing(list,"")
+for v in list:
+    try:    
+        dirlist=os.listdir(path+v)
+        listing(dirlist,v)
+        print('found a folder',v)
+    except:
+        pass
+
+ 
+
 ran=random.randint(0,len(wall))
+path=path+'{}'
 while True:
-    time.sleep(5)
-    ran=random.randint(0,len(wall))
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, "E:\Wallpapers\{}".format(wall[ran]) , 0)
+    time.sleep(t)
+    ran=random.randint(0,len(wall))-1
+    
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, str(path).format(wall[ran]) , 0)
+    
