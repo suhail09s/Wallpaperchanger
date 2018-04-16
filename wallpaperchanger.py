8b25477d7ade348f7b68235010b2fee30ecb0153
suhail09s <killer.x22@gmail.com>
2018-04-16 13:37:25 +0300

    fixed empty folder bug and gif format support
    +fixed empty folder bug.
    +added support for gif format.
    
    to fix:
    -the script only reads one level of subfolders.
    -select multiple folders .

--- wallpaperchanger.py
+++ wallpaperchanger.py
@@ -3,16 +3,18 @@ import os
 import threading
 import random
 import time
+import win32file
 global wall
 wall=[]
 global path
 global list
-path='E:\Wallpapers\\'
-t=0.5 # the duration in seconds
+path='c:\\' # the path has to end with  (\\)
+t=5           # the duration in seconds
 def listing(list,v):
     for a in range (len(list)):
-        if str(list[a]).split('.')[-1]=='jpg' or str(list[a]).split('.')[-1]=='png':
+        if str(list[a]).split('.')[-1]=='jpg' or str(list[a]).split('.')[-1]=='png' or str(list[a]).split('.')[-1]=='gif':
             wall.append(v+'\\'+list[a])
+start=time.time()
 list=os.listdir(path)
 
 listing(list,"")
@@ -24,10 +26,14 @@ for v in list:
     except:
         pass
 
- 
-
+print (len(wall),'Wallpapers were found')
 ran=random.randint(0,len(wall))
 path=path+'{}'
+print (time.time()-start)
+if len(wall)==0:
+    print('No wallpapers were found, please check.')
+    exit(0)
+
 while True:
     time.sleep(t)
     ran=random.randint(0,len(wall))-1
